1d8802165f7ee1cbab7d4f07a2818f73497becfa
Andres Sotomayor <warenwenaonda@gmail.com>
2025-06-09 01:00:11 -0400

    V1.0 Proyecto recién creado, nada modificado.

---
+++ src/main/java/com/example/app/PlataformaVotaciones/PlataformaVotacionesApplication.java
@@ -0,0 +1,13 @@
+package com.example.app.PlataformaVotaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaVotacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaVotacionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/app/PlataformaVotaciones/PlataformaVotacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app.PlataformaVotaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaVotacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
