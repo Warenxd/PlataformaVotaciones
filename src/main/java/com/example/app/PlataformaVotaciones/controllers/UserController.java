37d1561fb5965569a1f2276cec9ff7d5b8813e6b
Andres Sotomayor <warenwenaonda@gmail.com>
2025-06-09 01:54:00 -0400

    V1.1 - Implementación de carpetas MVC - Primera vista "/view" agregada. - Thymeleaf añadido a pom.xml - JPA comentado en pom.xml

---
+++ src/main/java/com/example/app/PlataformaVotaciones/controllers/UserController.java
@@ -0,0 +1,18 @@
+package com.example.app.PlataformaVotaciones.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/view") //Seleccionamos la ruta, al acceder a esa url, mostrará la información
+                        // de este metodo.
+    public String view(Model model){
+        model.addAttribute("title", "PLATAFORMA DE VOTACIONES \"VOTASEGURO");
+        model.addAttribute("message", "Bienvenido a VotaSeguro");
+        return "view";   // Devuelve y tiene que devolver sí o sí la plantilla que hicimos en_templates.
+    }
+}
