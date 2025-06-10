02aac78daf480794ff7b2fc52e1a7d7d20e10c59
Andres Sotomayor <warenwenaonda@gmail.com>
2025-06-10 11:02:56 -0400

    V1.3.1 NOTAS DE ACTUALIZACIÓN: - Se cambió el nombre de la vista "view.xml" por "menu.xml". En base a este cambio de nombre se cambió también el GetMapping de UserController en el metodo view para que haga un return a "menu"

--- src/main/java/com/example/app/PlataformaVotaciones/controllers/UserController.java
+++ src/main/java/com/example/app/PlataformaVotaciones/controllers/UserController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
-    @GetMapping({"/view", "/"}) //Seleccionamos la ruta, al acceder a esa url, mostrará la información
+    @GetMapping({"/menu", "/"}) //Seleccionamos la ruta, al acceder a esa url, mostrará la información
                         // de este metodo.
     public String view(Model model){
         model.addAttribute("title", "PLATAFORMA DE VOTACIONES \"VOTASEGURO");
         model.addAttribute("message", "Bienvenido a VotaSeguro");
-        return "view";   // Devuelve y tiene que devolver sí o sí la plantilla que hicimos en_templates.
+        return "menu";   // Devuelve y tiene que devolver sí o sí la plantilla que hicimos en_templates.
     }
 }
